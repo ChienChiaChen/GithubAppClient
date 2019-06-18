ea0e0930ef1a8994a1a3acd6dada2b9570e29fae
ChiaChen <s124202468@gmail.com>
2019-06-19 01:26:57 +0800

    Add toolbar.

--- app/src/main/java/com/chiachen/gihubappclient/MainActivity.kt
+++ app/src/main/java/com/chiachen/gihubappclient/MainActivity.kt
@@ -2,11 +2,13 @@ package com.chiachen.gihubappclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chiachen.gihubappclient.util.extension.setLightStatusBar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setLightStatusBar()
         setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/chiachen/gihubappclient/util/extension/BuildVersion.kt
@@ -0,0 +1,31 @@
+package com.chiachen.gihubappclient.util.extension
+
+import android.os.Build
+
+fun isLollipop_MR_1(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1
+}
+
+fun isMarshmallow(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+}
+
+fun isNougat(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+}
+
+fun isNougat_MR1(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1
+}
+
+fun isOreo(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+}
+
+fun isOreo_MR1(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1
+}
+
+fun isP(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
+}
