3f5a1d6bc5c61638a6ce0633e29379c4f3756173
JasonChien <jasonchien@kkstream.com>
2020-05-05 17:52:12 +0800

    Remove redundant code.

--- app/src/main/java/com/chiachen/githubappclient/di/AppModule.kt
+++ app/src/main/java/com/chiachen/githubappclient/di/AppModule.kt
@@ -3,7 +3,6 @@ package com.chiachen.githubappclient.di
 import android.app.Application
 import android.content.Context
 import android.content.res.Resources
-import android.net.ConnectivityManager
 import com.chiachen.githubappclient.App
 import com.chiachen.githubappclient.di.qualifier.ApplicationContext
 import dagger.Module
@@ -21,9 +20,4 @@ class AppModule(private val app: App) {
 
     @Provides
     internal fun provideApplication(): Application = app
-
-    @Provides
-    fun provideConnectivityManager(): ConnectivityManager {
-        return app.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    }
 }
