6e70cbdd7ff807c9adb744605e2305a719c3c867
JasonChien <jasonchien@kkstream.com>
2020-05-05 17:52:12 +0800

    Remove redundant code.

--- app/src/main/java/com/chiachen/githubappclient/di/AppModule.kt
+++ app/src/main/java/com/chiachen/githubappclient/di/AppModule.kt
@@ -4,11 +4,8 @@ import android.app.Application
 import android.content.Context
 import android.content.res.Resources
 import android.net.ConnectivityManager
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ProcessLifecycleOwner
 import com.chiachen.githubappclient.App
 import com.chiachen.githubappclient.di.qualifier.ApplicationContext
-import com.chiachen.githubappclient.di.qualifier.ProcessLifecycle
 import dagger.Module
 import dagger.Provides
 
@@ -24,12 +21,6 @@ class AppModule(private val app: App) {
 
     @Provides
     internal fun provideApplication(): Application = app
-
-    @Provides
-    @ProcessLifecycle
-    internal fun provideAppLifecycle(): Lifecycle {
-        return ProcessLifecycleOwner.get().lifecycle
-    }
 
     @Provides
     fun provideConnectivityManager(): ConnectivityManager {
--- app/src/main/java/com/chiachen/githubappclient/di/qualifier/ProcessLifecycle.kt
+++
@@ -1,6 +0,0 @@
-package com.chiachen.githubappclient.di.qualifier
-
-import javax.inject.Qualifier
-
-@Qualifier
-annotation class ProcessLifecycle
